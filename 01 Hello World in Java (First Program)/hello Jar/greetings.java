69c220fcd46b80335c9b2e57d2bbc352eae80a86
kapete23 <kevingokils97@gmail.com>
2021-12-23 23:32:58 +0700

    Menambahkan file .pdf dan .java pada folder <hello Jar>

---
+++ 01 Hello World in Java (First Program)/hello Jar/greetings.java
@@ -0,0 +1,8 @@
+class greetings { //Nama kelas ini merupakan acuan dari nama hasil compile.
+    //Jika source code ini di-compile akan menghasilkan file dengan nama
+    //hello.class.
+    public static void main(String[] args) { //Main Method
+        System.out.println("Hello, Word!");
+        System.out.println("Hai Kevin :)");
+}
+}
